9043846332b38c37c19480a0ba1f5ee3706fb34f
lqy <luoqy3@ziroom.com>
2018-05-14 12:14:42 +0800

    git push

---
+++ podtest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  podtest
+//
+//  Created by lqy on 2018/5/14.
+//  Copyright © 2018年 lqy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ podtest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  podtest
+//
+//  Created by lqy on 2018/5/14.
+//  Copyright © 2018年 lqy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
